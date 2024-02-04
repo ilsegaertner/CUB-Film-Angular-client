72458d6c0b79bd1255d4fab48f0643e72eba68dc
Ilse Gaertner <joachimpruegl@gmail.com>
2024-02-04 14:33:11 -0600

    Auto-generated commit

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-WG5LFDLC.mjs";import"./chunk-2LDK26EH.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-NWPDOJDH.mjs";import"./chunk-W7SNZPHP.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
