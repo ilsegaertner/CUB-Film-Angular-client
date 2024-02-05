7560a4d1342e035eced8e866ea992b2afa05573d
Ilse Gaertner <joachimpruegl@gmail.com>
2024-02-05 10:43:44 -0600

    Auto-generated commit [ci skip]

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-NWPDOJDH.mjs";import"./chunk-W7SNZPHP.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-UO2JOM7M.mjs";import"./chunk-W7SNZPHP.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
